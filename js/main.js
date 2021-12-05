5dc42e6e007b4f3b186ddc44cbe5341db1c90574
Mario Zalazar <mariojavierzalazar@gmail.com>
2021-12-04 23:29:58 -0300

    aplicamos DOM

--- js/main.js
+++ js/main.js
@@ -1,86 +1,58 @@
 //variables
-let continuarCargando;
+let continueBuying;
 let subtotal = 0;
-let total = 0;
-let codigoDescuento;
+let descCode;
+let tablaCarrito = document.getElementById('tablaCarrito');
+let tablaProductos = document.getElementById('listaProductos');
 
 //arrays
-const listaDeProductos = [];
-const listaDelCarrito = [];
-const carrito = [];
+const productList = [];
+const cart = [];
 
 //Fuciones
-const descuento = (valor) =>{
+const desc = (value) => {
     total = subtotal
-    return total *= valor;
-}
-
-//Objetos 
-class Producto {
-    constructor(id, nombre, valor) {
-        this.id = id,
-        this.nombre = nombre,
-        this.precioPorUnidad = valor;
-    }
-    precioConIva(){this.precioPorUnidad *= 1.21}
+    return total *= value;
 };
-//Creando Objetos
-const productos = [
-    new Producto(1,'Unidad tactica',7500),
-    new Producto(2,'Unidad de mando',5000),
-    new Producto(3,'Vehiculo',11000),
-    new Producto(4,'Tablero de juego',8000),
-    new Producto(5,'Set de dados',4500)
-];
+const saveLocal = (key, value) => {
+    localStorage.setItem(key, value)
+};
 
 //creando plantilla de lista de productos
-for (const producto of productos) {
-    producto.precioConIva();
-    listaDeProductos.push(`ID: ${producto.id} - ${producto.nombre} $ ${producto.precioPorUnidad}`);
+for (const product of products) {
+    product.priceIva();
+    productList.push(`ID: ${product.id} - ${product.name} $ ${product.price}`);
 }
+
+//Verificando si hay productos de manera local
+if ('cartList' in localStorage) {
+    const saveLocalProduct = JSON.parse(localStorage.getItem('cartList'));
+    console.log(saveLocalProduct)
+}
+
+
 
 do {
     //El usuario selecciona el producto por ID
-    let productoSeleccionado = parseInt(prompt(`Ingrese el producto a comprar, indicando su ID\n${listaDeProductos.join('\n')}`));
+    let productSelected = parseInt(prompt(`Ingrese el producto a comprar, indicando su ID\n${productList.join('\n')}`));
     //validamos que el usuarios cargue un valor de ID valido y no un caracter
-    while (productoSeleccionado < 1 || productoSeleccionado > productos.length || /\D/.test(productoSeleccionado)){
-        productoSeleccionado = parseInt(prompt(`Ingreso un valor erroneo\nFavor de  seleccionar el producto a comprar, indicando su ID\n${listaDeProductos.join('\n')}`));
+    while (productSelected < 1 || productSelected > products.length || /\D/.test(productSelected)) {
+        productSelected = parseInt(prompt(`Ingreso un valor erroneo\nFavor de  seleccionar el producto a comprar, indicando su ID\n${productList.join('\n')}`));
     }
-    //Mostramos el producto seleccionado y preguntamos cuantas unidades desea.
-    let nombreSeleccionado = productos.find(elemento => elemento.id === productoSeleccionado).nombre
-    let cantidadDeProductos = parseInt(prompt(`Seleccionastes ${nombreSeleccionado}.\n¿Cuantas unidades desea comprar?`));
-    for (let i = 0; i < cantidadDeProductos; i++) {
-        carrito.push((productos.find(elemento => elemento.id === productoSeleccionado)));
-    }
+    cart.push((products.find(elemento => elemento.id === productSelected)));
     //preguntamos si quiere continuar cargando
-    continuarCargando = prompt('¿Desea agregar otro producto a su carrito?\nIndicar "Si" para cargar un nuevo producto o "No" para continuar con la compra').toLowerCase();
-} while (continuarCargando != "no");
+    continueBuying = prompt('¿Desea agregar otro producto a su carrito?\nIndicar "Si" para cargar un nuevo producto o "No" para continuar con la compra').toLowerCase();
+} while (continueBuying != "no");
 
+let total = document.getElementById('tablaCarrito');
 
-for (const elementoDelCarrito of carrito) {
-    listaDelCarrito.push(`${elementoDelCarrito.nombre} $ ${elementoDelCarrito.precioPorUnidad}`);
-    subtotal += elementoDelCarrito.precioPorUnidad;
+for (const produtCart of cart) {
+    subtotal += produtCart.price;
+    let trLista = document.createElement('tr');
+    trLista.innerHTML = `<tr><td>${produtCart.name}</td><td>$${produtCart.price}</td></tr>`;
+    tablaProductos.appendChild(trLista);
 }
 
-//Solicitamos al usuario indicar un codigo de descuento
-codigoDescuento = prompt('A continuacion podra ingresar alguno de los siguientes codigos de descuentos:\nFF2021\nAN2022').toUpperCase();
-
-switch (codigoDescuento) {
-    case 'FF2021':
-        total = descuento(0.90);
-        alert(`Su carrito se compone de\n${listaDelCarrito.join('\n')}\nEl subtotal es de $ ${subtotal}\nSe aplico un descuento del 10% por el codigo ${codigoDescuento}\ny su total es de $ ${total}\nPresione Aceptar para finalizar la compra`)
-        break;
-    case 'AN2022':
-        total = descuento(0.80);
-        alert(`Su carrito se compone de\n${listaDelCarrito.join('\n')}\nEl subtotal es de $ ${subtotal}\nSe aplico un descuento del 20% por el codigo ${codigoDescuento}\ny su total es de $ ${total}\nPresione Aceptar para finalizar la compra`)
-        break;
-    default:
-        alert('No se aplicaron descuentos');
-        alert(`Su carrito se compone de\n${listaDelCarrito.join('\n')}\nEl total es de $ ${subtotal}\nPresione Aceptar para finalizar la compra`)
-        break;
-}
-
-alert('Muchas Gracias por su compra');
-
-
-
+let tbodyTotal = document.createElement('tbody');
+tbodyTotal.innerHTML = `<tr><th>Subtotal</th><th>$ ${subtotal}</th></tr>`;
+total.appendChild(tbodyTotal);
